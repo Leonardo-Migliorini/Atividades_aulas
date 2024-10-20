94ad407db139c9ca110f17a4d5c98d0927b2c075
Leonardo Migliorini <leonardopriormigliorini@gmail.com>
2024-10-19 21:48:10 -0300

    alguma att esquecida

--- Analise_Regressao/Trabalho Final/Análise descritivas dos dados.R
+++ Analise_Regressao/Trabalho Final/Análise descritivas dos dados.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 load("Dados//Models_Data.RData")
 dados_1 <- Models_Data
 dados <- Models_Data |> 
@@ -5,13 +7,20 @@ dados <- Models_Data |>
     GHGC = log(GHGC)
   ) |>
   dplyr::select(
-    -c(1)
+    -c(1,6)
   )
 
-shapiro.test(CO2)
+attach(dados)
+
+shapiro.test(GHGC)
+summary(GHGC)
 
 cor_dados <- cor(dados) 
-corrplot::corrplot(cor_dados, 
+
+pdf(file = "Corrplot.pdf",width = width, height = height,family = "Times")
+par(mar=c(mar_b, mar_e, mar_c, mar_d)) 
+par(mgp=c(dist_text, dist_tick, 0))
+plot(corrplot::corrplot(cor_dados, 
                    type = "upper", 
                    method = "color", 
                    col = RColorBrewer::brewer.pal(n = 8, name = "RdYlBu"), # Color palette
@@ -20,5 +29,19 @@ corrplot::corrplot(cor_dados,
                    tl.srt = 45, # Text label rotation
                    number.cex = 0.7, # Size of the correlation coefficient numbers
                    cl.cex = 0.7, # Size of the color legend text
-                   title = "Gráfico de correlação das variáveis", 
-                   mar = c(0,0,1,0)) # Title margin
+                   mar = c(0,0,1,0))) # Title margin
+dev.off()
+
+
+
+pdf(file = "Histograma.pdf",width = width, height = height,family = "Times")
+par(mar=c(mar_b, mar_e, mar_c, mar_d)) 
+par(mgp=c(dist_text, dist_tick, 0))
+plot(ggplot(data = dados, aes(x = GHGC)) +
+  geom_histogram(binwidth = 0.5, fill = "blue", color = "black", alpha = 0.7) +
+  labs(x = "log(GHGC)",
+       y = "Frequência") +
+  ggplot2::theme_bw() +
+  ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
+                 panel.grid.minor = ggplot2::element_blank()))
+dev.off()
